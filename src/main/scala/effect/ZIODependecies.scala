51e61f4f01473a7d0a1b184029fd4b1641acc8ee
mudriyjo <mudriyjo@gmail.com>
2024-05-03 19:13:27 +0400

    add example with standard zio services

--- src/main/scala/effect/ZIODependecies.scala
+++ src/main/scala/effect/ZIODependecies.scala
@@ -1,8 +1,10 @@
 package effect
 
-import zio._
-import effect.services._
-import effect.services.entity._
+import zio.*
+import effect.services.*
+import effect.services.entity.*
+
+import java.time.temporal.ChronoUnit
 
 object ZIODependecies extends ZIOAppDefault {
   
@@ -15,11 +17,9 @@ object ZIODependecies extends ZIOAppDefault {
         + Show 2 type of composition in live horizontal/vertical
     + 5. Prepare programm
     + 6. Provide dependencies
-    7. Show ZLayer.Debug funcationality for dependency graph
+    + 7. Show ZLayer.Debug funcationality for dependency graph
     8. Show Standart ZLayer services, such as: Clock, Random, System, Console
      */
-
-   
 
     val subscriptionServices: ZLayer[Any, Nothing, UserSubscriptionServices] = 
         (ConnectionPool.live(10) >>> UserDatabase.live ++ UserEmail.live)
@@ -31,9 +31,21 @@ object ZIODependecies extends ZIOAppDefault {
        _ <- subscribeService.subscribe(User("John", "john@gmail.com"))
     } yield()
     
-    
+    val program_v2 = for {
+        time <- Clock.currentTime(ChronoUnit.MILLIS)
+        _ <- Console.printLine(s"seconds are: ${time}")
+        _ <- Console.printLine(s">>>>>>>>>>>>>>>>>>>>>")
+        _ <- program
+        _ <- Console.printLine(s">>>>>>>>>>>>>>>>>>>>>")
+        time <- Clock.currentTime(ChronoUnit.MILLIS)
+        _ <- Console.printLine(s"seconds after call are: ${time}")
+        num <- Random.nextIntBetween(0, 100)
+        _ <- Console.printLine(s"random number is: ${num}")
+        envValue <- System.env("Test")
+        _ <- Console.printLine(s"Test value is: ${envValue}")
+    } yield()
 
-    def run = program.provide(
+    def run = program_v2.provide(
         // subscriptionServices
         UserSubscriptionServices.live,
         UserEmail.live,
